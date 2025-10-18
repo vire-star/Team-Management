2ab574608585de19c5a1cbfa0c5e5f3e518c5f4d
Virendra pratap <virendrapratap8744@gmail.com>
2025-10-18 22:14:30 +0530

    error checking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import AppRoutes from "./routes";
 
 function App() {
+  // Frontend - Console me print karo
+console.log('API_URL:', import.meta.env.VITE_API_URL)
+console.log('Full login URL:', `${import.meta.env.VITE_API_URL}/api/auth/login`)
+
   return <AppRoutes />;
 }
 
